81cbedc1d9c0d7eb45f59286c79ff74381de23fa
abunjinggg <158477718+abunjinggg@users.noreply.github.com>
2024-02-10 20:15:58 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<?php include 'menubar.php'; ?>
+<?php include 'sidebar.php'; ?>
+<main>
+<?php include 'main-body.php'; ?>
+</main>
+<?php include 'footer.php'; ?>
+
